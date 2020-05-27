05912862d944248c852033a003aecdc6d660727f
marc-matt <62201769+marc-matt@users.noreply.github.com>
2020-05-27 17:42:54 +0200

    Update esercizio-2020-5-22-ping-pong.c

--- src/esercizio-2020-5-22-ping-pong.c
+++ src/esercizio-2020-5-22-ping-pong.c
@@ -1,129 +1,89 @@
-#include <stdio.h>
+/*
+    Author: M.Marconi
+    Date:   27th May 2020
+    Status: DONE
+        PingPong.c
+*/
+
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <limits.h>
 #include <errno.h>
 #include <semaphore.h>
-#include <sys/types.h>
+#include <pthread.h>
 #include <sys/wait.h>
-#include <sys/fcntl.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define MAX_VALUE 1000000
 
+int pipe_fdA[2];
+int pipe_fdB[2];
 int globalCounter;
-sem_t sem;
 
-void process_1(int * pipe_fd_1[], int * pipe_fd_2[]){
-    close(pipe_fd_1[0]); // Close read end of pipe_1
-    close(pipe_fd_2[1]); // Close write end of pipe_2
-    int * buffer_r = calloc(1, sizeof(int));
-    int * buffer_w = calloc(1, sizeof(int));
-    int loc;
+int main(int argc, char * argv[]){
     int res;
-    while(globalCounter != MAX_VALUE){
-        if(sem_wait(&sem) == -1){
-            perror("sem_wait()");
-            exit(EXIT_FAILURE);
-        }
-        //FASE CRITICA
-        res = read(pipe_fd_2[0], buffer_r, sizeof(int));
-        if(res == -1){
-            perror("read()");
-            exit(EXIT_FAILURE);
-        }
-        if(*buffer_r == globalCounter){
-            printf("correct\n");
-        }
-        loc = globalCounter;
-        loc++;
-        globalCounter = loc;
-        *buffer_w = loc;
-        res = write(pipe_fd_1[1], buffer_w, sizeof(int));
-        if(res == -1){
-            perror("write()");
-            exit(EXIT_FAILURE);
-        }
-        //FASE CRITICA
-        if(sem_post(&sem) == -1){
-            perror("sem_post()");
-            exit(EXIT_FAILURE);
-        }
-         printf("[PARENT]: globalCounter incremented to: %d\n", globalCounter);
+    if(pipe(pipe_fdA) == -1){
+        perror("pipe()");
+        exit(EXIT_FAILURE);
     }
-}
-void process_2(int * pipe_fd_1[], int * pipe_fd_2[]){
-    close(pipe_fd_1[1]); // Close write end of pipe_1
-    close(pipe_fd_2[0]); // Close read end of pipe_2
-    int loc;
-    int * buffer_r = calloc(1, sizeof(int));
-    int * buffer_w = calloc(1, sizeof(int));
-    int res;
-    while(globalCounter != MAX_VALUE){
-        if(sem_wait(&sem) == -1){
-            perror("sem_wait()");
-            exit(EXIT_FAILURE);
-        }
-        //FASE CRITICA
-        res = read(pipe_fd_1[0], buffer_r, sizeof(int));
-        if(res == -1){
-            perror("read()");
-            exit(EXIT_FAILURE);
-        }
-        if(*buffer_r == globalCounter){
-            printf("correct\n");
-        }
-        loc = globalCounter;
-        loc++;
-        globalCounter = loc;
-        *buffer_w = loc;
-        res = write(pipe_fd_2[1], buffer_w, sizeof(int));
-        if(res == -1){
-            perror("write()");
-            exit(EXIT_FAILURE);
-        }
-        //FASE CRITICA
-        if(sem_post(&sem) == -1){
-            perror("sem_post()");
-            exit(EXIT_FAILURE);
-        }
-        printf("[CHILD]: globalCounter incremented to: %d\n", globalCounter);
-    }
-}
-
-int main(int argc, char*argv){
-    int pipe_fd_1[2];
-    int pipe_fd_2[2];
-    int res;
-    int s;
-    if (pipe(pipe_fd_1) == -1) {
-		perror("pipe()");
-		exit(EXIT_FAILURE);
-	}
-    if (pipe(pipe_fd_2) == -1) {
-		perror("pipe()");
-		exit(EXIT_FAILURE);
-	}
-    if(s = sem_init(&sem, 0,1) == -1){
-        perror("sem_init()");
+    if(pipe(pipe_fdB) == -1){
+        perror("pipe()");
         exit(EXIT_FAILURE);
     }
     switch(fork()){
-        case -1: /* Error */
+        case -1:
             perror("fork()");
             exit(EXIT_FAILURE);
             break;
-        case 0: /* Child process */
-            process_2(&pipe_fd_1, &pipe_fd_2);
+        case 0:
+            close(pipe_fdA[1]);
+            close(pipe_fdB[0]);
+            while(globalCounter < MAX_VALUE){
+                res = read(pipe_fdA[0], &globalCounter, sizeof(int));
+                if(res == -1){
+                    perror("read()");
+                    exit(EXIT_FAILURE);
+                }
+                if(res == 0){
+                    printf("EOF\n");
+                    break;
+                }
+                globalCounter++;
+                res = write(pipe_fdB[1], &globalCounter, sizeof(int));
+                if(res == -1){
+                    perror("write()");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            printf("[CHILD] Fuori da while.\n");
+            exit(EXIT_SUCCESS);
+        default:
+            ;
+    }
+    close(pipe_fdA[0]);
+    close(pipe_fdB[1]);
+    while(globalCounter < MAX_VALUE){
+        res = write(pipe_fdA[1], &globalCounter, sizeof(int));
+        if(res == -1){
+            perror("write()");
+            exit(EXIT_FAILURE);
+        }
+        res = read(pipe_fdB[0], &globalCounter, sizeof(int));
+        if(res == -1){
+            perror("read()");
+            exit(EXIT_FAILURE);
+        }
+        if(res == 0){
+            printf("EOF\n");
             break;
-        default: /* Parent process */
-            process_1(&pipe_fd_1, &pipe_fd_2);
+        }
     }
-    printf("final globalCounter = %d\n", globalCounter);
-    if(s = sem_destroy(&sem) == -1){
-        perror("sem_init()");
-        exit(EXIT_FAILURE);
-    }
-    exit(EXIT_SUCCESS);
+    printf("[PARENT] Fuori da while.\n");
+    printf("Final globalCounter value: %d\n", globalCounter);
+    printf("Bye!\n");
+    return 0;
 }
